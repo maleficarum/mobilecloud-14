a99d5600839296390bd6997870c0c3a785556c5d
Oscar Ivan Hernandez Ventura <oscar@angellore.com.mx>
2014-09-19 02:07:19 -0500

    Test testAddVideoMetadata passed

--- assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/MyController.groovy
+++ assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/MyController.groovy
@@ -1,9 +1,13 @@
 package org.magnum.mobilecloud.video
 
+import org.magnum.mobilecloud.video.repository.Video
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
+
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Date: 19/09/14 01:36
@@ -13,6 +17,18 @@ import org.springframework.web.bind.annotation.ResponseBody
 class MyController {
 
     def videos = [:];
+    private static final AtomicLong currentId = new AtomicLong(0L);
+
+    @RequestMapping(value = "/video", method = RequestMethod.POST)
+    public @ResponseBody Video postVideo(@RequestBody Video v){
+        if(!v.getId()) {
+            v.setId(currentId.incrementAndGet())
+        }
+
+        videos[v.getId()] = v
+
+        v
+    }
 
     @RequestMapping(value = "/video", method = RequestMethod.GET)
     public @ResponseBody List getVideo(){
